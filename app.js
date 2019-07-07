334c3358cf1f550bab2488b08e91dcc8cbc501df
Marcus Foertsch <marcus@foertschemail.com>
2019-07-07 17:04:59 -0500

    Use chalk module.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const validator = require('validator');
 const getNotes = require('./notes');
+const chalk = require('chalk');
 
 console.log(getNotes);
 
 console.log(validator.isURL('https://www.foertschemail.com'));
+
+console.log(chalk.green('Success!'));
