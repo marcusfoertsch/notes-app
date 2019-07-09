296d51c8f976c5675b9bdbdd11b8b25d16123c70
Marcus Foertsch <marcus@foertschemail.com>
2019-07-08 20:02:04 -0500

    Options for "add" command.

--- app.js
+++ app.js
@@ -1,9 +1,58 @@
-const validator = require('validator');
-const getNotes = require('./notes');
 const chalk = require('chalk');
+const yargs = require('yargs');
+const getNotes = require('./notes');
 
-console.log(getNotes);
+//Customize yargs version
+yargs.version('1.1.0');
 
-console.log(validator.isURL('https://www.foertschemail.com'));
+// add, remove, read, list
 
-console.log(chalk.inverse.red('Error!'));
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+      title: {
+          describe: 'Note title',
+          demandOption: true,
+          type: 'string'
+      },
+      body: {
+          describe: 'Note body',
+          demandOption: true,
+          type: 'string'
+      }
+    },
+    handler: function (argv) {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body);
+    }
+});
+
+// Create remove command
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing the note')
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'List the notes',
+    handler: function () {
+        console.log('Listing the notes')
+    }
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function() {
+        console.log('Reading the note')
+    }
+});
+
+yargs.parse();
