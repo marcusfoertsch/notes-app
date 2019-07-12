4234fd48ba29dc21eaa5ddc0fe09ac3c506e2d97
Marcus Foertsch <marcus@foertschemail.com>
2019-07-12 07:45:28 -0500

    Use ES6 functions.

--- notes.js
+++ notes.js
@@ -1,16 +1,13 @@
 const fs = require('fs');
 const chalk = require('chalk');
 
-const getNotes = function () {
-    return 'Your notes...';
-};
+const getNotes = () => 'Your notes...';
 
-const addNote = function (title, body) {
+const addNote = (title, body) => {
     const notes = loadNotes();
 
-    const duplicateNotes = notes.filter(function (note) {
-        return note.title === title;
-    });
+    const duplicateNotes = notes.filter((note) => note.title === title);
+
 
     if (duplicateNotes.length === 0) {
         notes.push({
@@ -26,12 +23,10 @@ const addNote = function (title, body) {
     }
 };
 
-const removeNote = function (title) {
+const removeNote = (title) => {
     const notes = loadNotes();
 
-    const notesToKeep = notes.filter(function (note) {
-        return note.title !== title;
-    })
+    const notesToKeep = notes.filter((note) => note.title !== title);
 
     if (notes.length > notesToKeep.length) {
         saveNotes(notesToKeep);
@@ -43,13 +38,13 @@ const removeNote = function (title) {
     }
 }
 
-const saveNotes = function (notes) {
+const saveNotes = (notes) => {
     const dataJSON = JSON.stringify(notes);
 
     fs.writeFileSync('notes.json', dataJSON);
 };
 
-const loadNotes = function () {
+const loadNotes = () => {
     try {
         const dataBuffer = fs.readFileSync('notes.json');
         const dataJSON = dataBuffer.toString();
